190508a9b63e4d9571784e7ab8ab989637e2013b
eliezer <eliezerassuncaocustodio@hotmail.com>
2024-06-23 18:08:25 -0300

    packages

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,8 +52,8 @@ android {
 dependencies {
 
     implementation("com.google.android.gms:play-services-auth:19.2.0")
-   /* implementation(project(":frajola:patches:brasilian_currency"))
-    implementation(project(":frajola:patches:google_sign_in"))*/
+    implementation(project(":frajola:patches:brasilian_currency"))
+    implementation(project(":frajola:patches:google_sign_in"))
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
--- frajola/patches/google_sign_in/build.gradle.kts
+++ frajola/patches/google_sign_in/build.gradle.kts
@@ -31,7 +31,6 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
-
     buildFeatures {
         compose = true
     }
@@ -42,39 +41,20 @@ android {
 
 dependencies {
 
-    implementation("com.google.android.gms:play-services-auth:19.2.0")
-
+    implementation("com.google.android.gms:play-services-auth:21.2.0")
     implementation("androidx.activity:activity-compose:1.9.0")
     implementation(platform("androidx.compose:compose-bom:2024.06.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-
-
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
-
+    
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-
 }
 
 publishing {
