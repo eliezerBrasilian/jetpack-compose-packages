a5f0f870b35a99f276075f0ad5a8f0dc222751ad
eliezer <eliezerassuncaocustodio@hotmail.com>
2024-07-14 21:45:25 -0300

    Configuração de publicação para pacotes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,7 @@ android {
 }
 
 dependencies {
+    implementation("com.google.android.gms:play-services-auth:19.2.0")
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
